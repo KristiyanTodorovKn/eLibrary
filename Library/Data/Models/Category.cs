6ceae1752c512cdc999587b6cd0f254592a30b67
Kris <atlantis_kn>
2022-10-24 22:17:38 +0300

    Added Library project

---
+++ Library/Data/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using MessagePack;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Library.Data.Models
+{
+    public class Category
+    {
+        
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public List<Book> Books { get; set; } = new List<Book>();
+    }
+}
---
+++ Library/Models/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class LoginViewModel
+    {
+        
+        public string UserName { get; set; }
+
+        
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
